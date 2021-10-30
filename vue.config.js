26b00b8af1dce315aefe26213ae8355f9e0cf98e
olgak169 <okrupnova@gmail.com>
2021-10-30 13:39:06 +0600

    config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/fm-timetracker/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/FMTimeTracker/' : '/',
 }
